ac8394b8fa24e86d28d90f2dabc8d6da46723baa
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-12 11:17:16 +0900

    solved 1030

---
+++ CodeUp/CodeUp1030.java
@@ -0,0 +1,17 @@
+package codeup; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1030 -> Main 으로 변경필요!
+public class CodeUp1030 {
+
+	public static void main(String[] args){
+		long d;
+		//Scanner 함수 사용
+		Scanner sc = new Scanner(System.in);
+		d = sc.nextLong();
+		sc.close();
+
+		System.out.printf("%d",d);
+	}
+}
