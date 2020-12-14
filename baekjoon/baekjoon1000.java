5a680fbe2cd3cb162b2ffda255de3954f5d25354
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-14 12:09:31 +0900

    solved baekjoon 1000

---
+++ baekjoon/baekjoon1000.java
@@ -0,0 +1,16 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon1000 -> Main 으로 변경
+public class baekjoon1000 {
+	public static void main(String[] args){
+		
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+b);
+	}
+
+}
