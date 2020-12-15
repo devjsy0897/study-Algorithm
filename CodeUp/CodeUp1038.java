508eece84dccb8eb8c8420614feb3a5ff36c623e
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-15 14:20:29 +0900

    solved code up 1038

---
+++ CodeUp/CodeUp1038.java
@@ -0,0 +1,20 @@
+package codeup; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1038 -> Main 으로 변경필요!
+public class CodeUp1038 {
+
+	public static void main(String[] args){
+		long num1,num2;
+		
+		//Scanner 함수 사용
+		Scanner sc = new Scanner(System.in);
+		num1 = sc.nextLong();
+		num2 = sc.nextLong();
+		sc.close();
+
+		System.out.println(num1+num2);
+		
+	}
+}
