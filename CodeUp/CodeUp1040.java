2e3827ed5ae6067cb001b0e4cdcd2b493d1275bf
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-16 21:04:00 +0900

    solved code up 1040

--- CodeUp/CodeUp1040.java
+++ CodeUp/CodeUp1040.java
@@ -2,7 +2,7 @@ package codeup; //제출 시 패키지 삭제
 
 import java.util.Scanner;
 
-//제출 시 클래스 명 CodeUp1039 -> Main 으로 변경필요!
+//제출 시 클래스 명 CodeUp1040 -> Main 으로 변경필요!
 public class CodeUp1040 {
 
 	public static void main(String[] args){
