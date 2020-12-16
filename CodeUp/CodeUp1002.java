483b2c86604214c6767eb0a612d9136c4e942019
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-16 21:20:17 +0900

    solved code up 1002

---
+++ CodeUp/CodeUp1002.java
@@ -0,0 +1,12 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1002 -> Main 으로 변경필요! 
+public class CodeUp1002 {
+
+    public static void main (String[] args) {
+    System.out.println("Hello World");
+    }
+}
