05ffac54c43babfdb66ab1585c91687c7eca47bf
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-29 20:28:41 +0900

    solved code up 1005

---
+++ CodeUp/CodeUp1005.java
@@ -0,0 +1,11 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+
+//제출 시 클래스 명 CodeUp1004 -> Main 으로 변경필요! 
+public class CodeUp1005 {
+
+    public static void main (String[] args) {
+    System.out.printf("\'Hello\'");
+    }
+}
