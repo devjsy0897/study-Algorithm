1955330d50d8dbe0ad2b26a6ff8ea9fd447fe7e2
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-29 20:25:41 +0900

    solved code up 1003

---
+++ CodeUp/CodeUp1003.java
@@ -0,0 +1,11 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+
+//제출 시 클래스 명 CodeUp1002 -> Main 으로 변경필요! 
+public class CodeUp1003 {
+
+    public static void main (String[] args) {
+    System.out.printf("Hello\nWorld");
+    }
+}
