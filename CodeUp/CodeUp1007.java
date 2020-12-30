097dcbcae27c44fa2b88724bc3940101b8ab590d
JeongSeongYeon <devjsy0897@gmail.com>
2020-12-30 22:07:59 +0900

    solved code up 1007

---
+++ CodeUp/CodeUp1007.java
@@ -0,0 +1,11 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+
+//제출 시 클래스 명 CodeUp1007 -> Main 으로 변경필요! 
+public class CodeUp1007 {
+
+    public static void main (String[] args) {
+    System.out.printf("\"C:\\Download\\hello.cpp\"");
+    }
+}
