0f5e255491e5a8522a09bafce1dd6b292d53fe02
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-08 22:11:58 +0900

    solved code up 1013

---
+++ CodeUp/CodeUp1013.java
@@ -0,0 +1,19 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1013 -> Main 으로 변경필요! 
+public class CodeUp1013 {
+
+    public static void main(String[] args) {
+
+    	
+    	Scanner sc = new  Scanner(System.in);
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	sc.close();
+    	System.out.printf("%d %d",a,b);
+    
+    }
+}
