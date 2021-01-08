702d4972296900b6742de4b73a38c6b8c00d92d9
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-08 22:15:24 +0900

    solved code up 1015

---
+++ CodeUp/CodeUp1015.java
@@ -0,0 +1,18 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1015 -> Main 으로 변경필요! 
+public class CodeUp1015 {
+
+    public static void main(String[] args) {
+
+    	
+    	Scanner sc = new  Scanner(System.in);
+    	float x = sc.nextFloat();
+    	sc.close();
+    	System.out.printf("%.2f",x);
+    
+    }
+}
