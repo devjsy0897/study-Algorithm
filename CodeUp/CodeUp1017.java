378ae22293ce9a9f4a7732ecdbea0fef40243684
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-15 17:07:36 +0900

    solved code up 1017

---
+++ CodeUp/CodeUp1017.java
@@ -0,0 +1,19 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1017 -> Main 으로 변경필요! 
+public class CodeUp1017 {
+
+    public static void main(String[] args) {
+
+    	
+    	Scanner sc = new  Scanner(System.in);
+    	int i = sc.nextInt();
+    	sc.close();
+    	System.out.println(i+" "+i+" "+i);
+    	
+    
+    }
+}
