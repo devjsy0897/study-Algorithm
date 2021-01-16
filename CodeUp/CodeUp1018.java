4c09b355edf9f8faffc88ad8f7c39cb63a374832
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-16 16:18:53 +0900

    solved code up 1018

---
+++ CodeUp/CodeUp1018.java
@@ -0,0 +1,19 @@
+//제출 시 패키지 삭제 
+package codeup;
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1018 -> Main 으로 변경필요! 
+public class CodeUp1018 {
+
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new  Scanner(System.in);
+    	String time = sc.next();
+    
+    	sc.close();
+    	System.out.println(time);
+    	
+    
+    }
+}
