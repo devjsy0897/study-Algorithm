d86276f006f54edc1cae0400c329135836532d0a
JeongSeongYeon <devjsy0897@gmail.com>
2021-01-19 14:08:24 +0900

    solved code up 1042

---
+++ CodeUp/CodeUp1042.java
@@ -0,0 +1,20 @@
+package codeup; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1042 -> Main 으로 변경필요!
+public class CodeUp1042 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		float a = sc.nextFloat();
+		float b = sc.nextFloat();
+		sc.close();
+		
+		
+		
+		System.out.println((int)(a/b));
+		
+	}
+}
