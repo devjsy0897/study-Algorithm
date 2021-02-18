599b065f176b097221a49392f54ffbef7da8c381
JeongSeongYeon <devjsy0897@gmail.com>
2021-02-18 21:05:53 +0900

    solved code up 1045

---
+++ CodeUp/CodeUp1045.java
@@ -0,0 +1,22 @@
+package codeup; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1042 -> Main 으로 변경필요!
+public class CodeUp1045 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.printf("%.2f",(float)a/b);
+	}
+}
