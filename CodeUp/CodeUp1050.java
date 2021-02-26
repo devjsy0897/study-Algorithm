5620cd9a347892fc265f7aecdbbe7e283245eccc
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-02-27 08:55:12 +0900

    solved code up 1050

---
+++ CodeUp/CodeUp1050.java
@@ -0,0 +1,19 @@
+package codeUp; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1050 -> Main 으로 변경필요!
+public class CodeUp1050 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		int c = (a == b) ? 1 : 0; 
+		System.out.println(c);
+	}
+}
+
