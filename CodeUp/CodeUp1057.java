971303902cc85bab6a9bc71547e398cdbf398e03
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-01 21:23:03 +0900

    solved code up 1057

---
+++ CodeUp/CodeUp1057.java
@@ -0,0 +1,22 @@
+package codeUp; //제출 시 패키지 삭제
+
+import java.util.Scanner;
+
+//제출 시 클래스 명 CodeUp1057 -> Main 으로 변경필요!
+public class CodeUp1057 {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		if(a==b){
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+	}
+}
+
