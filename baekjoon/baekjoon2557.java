ecccfa8bc32b0fc32d8c44e512a09f2b103cb068
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-02 22:07:28 +0900

    solved baekjoon 2557

---
+++ baekjoon/baekjoon2557.java
@@ -0,0 +1,12 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon1000 -> Main 으로 변경
+public class baekjoon2557 {
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!");
+	}
+
+}
