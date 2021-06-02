98487ae5e3e82711d58b8b065416e892d0cb8b76
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-02 22:10:04 +0900

    Update baekjoon10718.java

--- baekjoon/baekjoon10718.java
+++ baekjoon/baekjoon10718.java
@@ -2,7 +2,7 @@ package baekjoon; //제출시 삭제
 
 import java.util.Scanner;
 
-//제출시 baekjoon1000 -> Main 으로 변경
+//제출시 baekjoon10718 -> Main 으로 변경
 public class baekjoon10718 {
 	public static void main(String[] args){
 		
