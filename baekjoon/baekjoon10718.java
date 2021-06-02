0ae4149e9b0bd0c256ce7fec868526458c72198f
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-02 22:09:22 +0900

    solved baekjoon 10718

---
+++ baekjoon/baekjoon10718.java
@@ -0,0 +1,13 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon1000 -> Main 으로 변경
+public class baekjoon10718 {
+	public static void main(String[] args){
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
