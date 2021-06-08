5596a43e54c06f26d18d7468a1c146141ac869a3
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-08 21:53:18 +0900

    solved baekjoon 1008

---
+++ baekjoon/baekjoon1008.java
@@ -0,0 +1,16 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon1008 -> Main 으로 변경
+public class baekjoon1008 {
+	public static void main(String[] args){
+		
+		double a,b;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextDouble();
+		b=sc.nextDouble();
+		System.out.println(a/b);
+	}
+
+}
