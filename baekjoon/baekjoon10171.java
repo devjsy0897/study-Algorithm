c9fae97c0d564335dc02c449726de35cfdc25f04
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-08 20:50:10 +0900

    solved baekjoon 10171

---
+++ baekjoon/baekjoon10171.java
@@ -0,0 +1,13 @@
+package baekjoon; //제출시 삭제
+
+//제출시 baekjoon10171 -> Main 으로 변경
+public class baekjoon10171 {
+	public static void main(String[] args){
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
