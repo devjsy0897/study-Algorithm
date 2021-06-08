8155ec49532a301040681ae6774890e2302ba196
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-08 21:00:20 +0900

    solved baekjoon 10172

---
+++ baekjoon/baekjoon10172.java
@@ -0,0 +1,15 @@
+package baekjoon; //제출시 삭제
+
+//제출시 baekjoon10172 -> Main 으로 변경
+public class baekjoon10172 {
+	public static void main(String[] args){
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+
+}
