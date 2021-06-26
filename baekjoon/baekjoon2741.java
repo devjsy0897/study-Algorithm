34aab190823f0aa8d1a62e862920a81a5d9296d8
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-06-26 09:21:39 +0900

    solved baekjoon 2741

---
+++ baekjoon/baekjoon2741.java
@@ -0,0 +1,18 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon2741 -> Main 으로 변경
+public class baekjoon2741 {
+	public static void main(String[] args){
+		
+		int a;
+		Scanner sc = new Scanner(System.in);	
+		a=sc.nextInt();
+		
+		for(int i=1;i<=a;i++){
+			System.out.println(i);
+		}
+	}
+
+}
