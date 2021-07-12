a12c1f0ec5f1eb7649eed9d0c267988b1ab09a59
devjsy0897 <69065695+devjsy0897@users.noreply.github.com>
2021-07-12 21:08:09 +0900

    solved baeckjoon 11021

---
+++ baekjoon/baekjoon11021.java
@@ -0,0 +1,20 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon11021 -> Main 으로 변경
+public class baekjoon11021 {
+	public static void main(String[] args){
+		
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextInt();
+		
+		for(int i=1;i<=a;i++){
+			b=sc.nextInt();
+			c=sc.nextInt();
+			System.out.println("Case #"+i+": "+(b+c));
+		}
+	}
+
+}
