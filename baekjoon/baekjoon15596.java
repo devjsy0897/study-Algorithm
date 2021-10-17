b6bfb6c3eb6ddb112840fb30c739fa655e679b99
devjsy0897 <devjsy0897@gmail.com>
2021-10-17 13:30:15 +0900

    solved baekjoon15596

---
+++ baekjoon/baekjoon15596.java
@@ -0,0 +1,13 @@
+package baekjoon; //제출시 삭제
+
+//제출시 baekjoon15596 -> Test 으로 변경
+public class baekjoon15596 {
+
+	long sum(int[] a){
+		long sum = 0;
+		
+		for(int i=0;i<a.length;i++)
+			sum+=a[i];
+		return sum;
+	}
+}
