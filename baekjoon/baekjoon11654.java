0d9cc3383a019a0e767fea557a2619a7d6acf657
devjsy0897 <devjsy0897@gmail.com>
2021-10-24 12:42:09 +0900

    solved baekjoon 11654

---
+++ baekjoon/baekjoon11654.java
@@ -0,0 +1,20 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon11654 -> Main 으로 변경
+public class baekjoon11654 {
+	public static void main(String[] args){
+		
+		
+		Scanner sc = new Scanner(System.in);	
+		char ch = sc.nextLine().charAt(0);
+		int num = (int)ch;
+		sc.close();
+		System.out.println(num);
+		
+		
+		
+	}
+
+}
