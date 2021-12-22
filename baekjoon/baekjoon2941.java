80e7ebbf9aede98fa370e74da565a0ecc691d4ab
devjsy0897 <devjsy0897@gmail.com>
2021-12-22 21:48:04 +0900

    solved baekjoon 2941

--- baekjoon/baekjoon2941.java
+++ baekjoon/baekjoon2941.java
@@ -6,46 +6,17 @@ import java.util.Scanner;
 public class baekjoon2941 {
 	public static void main(String[] args){
 		
-		String str;
 		Scanner sc = new Scanner(System.in);
-
-		str = sc.nextLine();
-		sc.close();
-
-		System.out.println(str);
-System.out.println("?? : "+str.indexOf("s="));
-
-			int cnt = 0;
-			int l = 0;
-			while(true){
-				if(str.indexOf("c=", l )!=-1){
-					l = str.indexOf("c=", l );
-				}else if(str.indexOf("c-", l )!=-1){
-					l = str.indexOf("c-", l );
-				}else if(str.indexOf("dz=", l )!=-1){
-					l = str.indexOf("dz=", l );
-				}else if(str.indexOf("d-", l )!=-1){
-					l = str.indexOf("d-", l );
-				}else if(str.indexOf("lj", l )!=-1){
-					l = str.indexOf("lj", l );
-				}else if(str.indexOf("nj", l )!=-1){
-					l = str.indexOf("nj", l );
-				}else if(str.indexOf("s=", l )!=-1){
-					l = str.indexOf("s=", l );
-				}else if(str.indexOf("z=", l )!=-1){
-					l = str.indexOf("z=", l );
-				}else{
-					l = -1;
-				}
-					
-				System.out.println(l);
-				if(l == -1){
-					System.out.println("탈출");
-					break;
-				}
-				l++;
-			}
-			System.out.println("cnt : "+cnt);
+	    String str=sc.nextLine().trim();
+	    sc.close();
+	    String[] arr= {"c=","c-","dz=","d-","lj","nj","s=","z="};
+	    
+	    for(int i=0;i<arr.length;i++) {
+	        if(str.contains(arr[i])) {
+	            str=str.replaceAll(arr[i]," ");
+	            }
+	        }   
+	    System.out.println(str.length());
 		
 	}
 
