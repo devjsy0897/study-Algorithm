fec95b7beb9beb4c29cba7c8d3a335629aec981e
devjsy0897 <devjsy0897@gmail.com>
2022-01-12 22:08:55 +0900

    solved baekjoon 1712

--- baekjoon/baekjoon1712.java
+++ baekjoon/baekjoon1712.java
@@ -2,32 +2,25 @@ package baekjoon; //제출시 삭제
 
 import java.util.Scanner;
 
-//제출시 baekjoon4344 -> Main 으로 변경
+//제출시 baekjoon1712 -> Main 으로 변경
 public class baekjoon1712 {
 	public static void main(String[] args){
 		
-		int cnt = 0;
 		Scanner sc = new Scanner(System.in);
-		cnt = sc.nextInt();
 		
-		for(int i=0;i<cnt;i++){
-			int cnt2 = sc.nextInt();
-			int[] cnt3 = new int[cnt2];
-			int sumCnt = 0;
-			for(int j=0;j<cnt3.length;j++){
-				cnt3[j] = sc.nextInt();
-				sumCnt += cnt3[j];
-			}
-			double avg = (double)sumCnt / cnt3.length;
-			double cnt4 = 0;
-			for(int j=0;j<cnt3.length;j++){
-				if(cnt3[j]>avg){
-					cnt4++;
-				}
-			}
-			System.out.printf("%.3f", 100.0 * cnt4 / cnt3.length);
-			System.out.println("%");
+		int a = sc.nextInt();	
+		int b = sc.nextInt();	
+		int c = sc.nextInt(); 	
+        sc.close();
+        
+		if (c <= b) {
+			System.out.println("-1");
+		} 
+		else {
+			System.out.println((a/(c-b))+1);
 		}
+		
+		
 		
 		
 	}
