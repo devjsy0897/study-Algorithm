3a5391d7fbe63300473e2fa4801703d3e94023b4
devjsy0897 <devjsy0897@gmail.com>
2022-03-14 06:54:10 +0900

    solved backjoon 18108

---
+++ baekjoon/baekjoon18108.java
@@ -0,0 +1,18 @@
+package baekjoon; //제출시 삭제
+
+import java.util.Scanner;
+
+//제출시 baekjoon18108 -> Main 으로 변경
+public class baekjoon18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year_thai = sc.nextInt();
+		sc.close();
+
+		int year_ac = year_thai-543;
+		System.out.println(year_ac);
+		
+		
+	}
+}
